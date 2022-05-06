0c75a8f765a5811a9b1d8a1e7bfee521611c8307
Egor <egor.golubev2003@gmail.com>
2022-05-06 21:07:49 +0300

    Initial commit

---
+++ header.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ashot on 06.05.2022.
+//
+
+#include "header.h"
---
+++ header.h
@@ -0,0 +1,68 @@
+
+#ifndef DZ9_HEADER_H
+#define DZ9_HEADER_H
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Property
+{
+private:
+    double worth;
+public:
+    Property(double worth)
+    {
+        this->worth = worth;
+        cout << this->worth << endl;
+    }
+    virtual double Tax()
+    {
+        return this->worth;
+    }
+    double GetWorth()
+    {
+        return worth;
+    }
+};
+class Apartment :public Property
+{
+public:
+    Apartment(double worth):Property(worth)
+    {
+
+    }
+    double Tax() override
+    {
+
+        return GetWorth() / 1000;
+    }
+};
+class Car :public Property
+{
+public :
+    Car(double worth) :Property(worth)
+    {
+
+    }
+    double Tax() override
+    {
+
+        return GetWorth() / 200;
+    }
+};
+class CountryHouse : public Property
+{
+public:
+    CountryHouse(double worth) :Property(worth)
+    {
+
+    }
+    double Tax() override
+    {
+
+        return GetWorth() / 500;
+    }
+};
+
+#endif //DZ9_HEADER_H
